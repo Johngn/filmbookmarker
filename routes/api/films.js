2bb9f3cc7789c6b8977e5c92509070b236450a88
John Gillan <jgillan@protonmail.com>
2020-12-14 22:20:27 +0100

    added client folder

--- routes/api/films.js
+++ routes/api/films.js
@@ -4,13 +4,33 @@ const router = express.Router();
 // Film model
 const Film = require("../../models/Film");
 
-// @route   GET api.films
+// @route   GET api/films
 // @desc    get all films
 // @access  Public
 router.get("/", (req, res) => {
   Film.find()
     .sort({ name: 1 })
     .then((films) => res.json(films));
+});
+
+// @route   POST api/films
+// @desc    create film
+// @access  Public
+router.post("/", (req, res) => {
+  const newFilm = new Film({
+    name: req.body.name,
+  });
+
+  newFilm.save().then((film) => res.json(film));
+});
+
+// @route   DELETE api/films
+// @desc    delete film
+// @access  Public
+router.delete("/:id", (req, res) => {
+  Film.findById(req.params.id)
+    .then((film) => film.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
