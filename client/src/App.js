86a2cfcb1374afc503a0b5d90c9c096b4f783f82
John Gillan <jgillan@protonmail.com>
2020-12-21 18:08:11 +0000

    added FilmCard component

--- client/src/App.js
+++ client/src/App.js
@@ -2,51 +2,65 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import Searchbox from "./components/Searchbox";
+import FilmCard from "./components/FilmCard";
 
 import "./App.css";
 
 class App extends Component {
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-    // console.log(this.state.value);
-  };
+    state = {
+        searchValue: "",
+        defaultURL:
+            "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0b94b1025e86742975b86c5a81513b54",
+        searchURL:
+            'https://api.themoviedb.org/3/search/movie?api_key=0b94b1025e86742975b86c5a81513b54&query="',
+        movies: [],
+    };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    // console.log("A name was submitted: " + this.state.value);
-
-    if (this.state.value && this.state.value !== "") {
-      console.log(this.state.value);
-      this.getMovies(this.state.search_url + this.state.value);
-
-      this.setState({ search_url: "" });
-    } else {
-      window.location.reload();
+    componentDidMount() {
+        this.getMovies(this.state.defaultURL);
     }
-  };
 
-  getMovies = (url) => {
-    axios
-      .get(url)
-      .then((res) => {
-        console.log(res.data.results[0].original_title);
+    handleChange = (e) => {
+        this.setState({ searchValue: e.target.value });
+    };
 
-        this.setState({});
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    handleSubmit = (e) => {
+        e.preventDefault();
 
-  render() {
-    return (
-      <div className="App">
-        <Searchbox
-          props={(this.handleChange, this.handleSubmit, this.getMovies)}
-        />
-      </div>
-    );
-  }
+        this.getMovies(this.state.searchURL + this.state.searchValue);
+    };
+
+    getMovies = (url) => {
+        axios
+            .get(url)
+            .then((res) => {
+                console.log(res.data.results);
+
+                this.setState({ movies: res.data.results });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    render() {
+        const { movies } = this.state;
+        return (
+            <div className="App">
+                <Searchbox
+                    handleChange={this.handleChange}
+                    handleSubmit={this.handleSubmit}
+                    getMovies={this.getMovies}
+                    searchValue={this.state.searchValue}
+                />
+                <main id="main">
+                    {movies.map((movie, i) => (
+                        <FilmCard key={movie.id} movie={movie} />
+                    ))}
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/components/FilmCard.js
+++ client/src/components/FilmCard.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 
 export default class FilmCard extends Component {
-  render() {
-    return (
-      <div>
-        {/* <img src=${IMG_PATH + poster_path} alt=${title}> */}
-        <div class="movie-info">
-          <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        <div class="overview">
-          <h3>Overview</h3>${overview}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const movie = this.props.movie;
+        return (
+            <div className="movie">
+                <img
+                    src={"http://image.tmdb.org/t/p/w1280" + movie.poster_path}
+                    alt={movie.title}
+                ></img>
+                <div className="movie-info">
+                    <h3>{movie.title}</h3>
+                    <span className="">{movie.vote_average}</span>
+                    <h4>{movie.release_date.slice(0, 4)}</h4>
+                </div>
+                {/* <div className="overview">
+                    <h3>Overview</h3>
+                    {movie.overview}
+                </div> */}
+            </div>
+        );
+    }
 }
