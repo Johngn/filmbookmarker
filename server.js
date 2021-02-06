cba4e865815014f661a9d38a01f93d95789fa00f
John Gillan <jgillan@protonmail.com>
2021-02-06 19:03:06 +0100

    preparing for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const films = require("./routes/api/films");
 const users = require("./routes/api/users");
@@ -27,6 +28,16 @@ mongoose
 app.use("/api/films", films);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    // set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
